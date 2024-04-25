d59f0e453bbbd9d852a513a72faa3b9476634ff2
Ravi <ravi.prasad@accolitedigital.com>
2024-04-25 11:46:45 +0530

    register done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './Components/landing/landing.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'landing',
+        component: LandingComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+    {
+        path: '',
+        component: LandingComponent
+    }
+];
