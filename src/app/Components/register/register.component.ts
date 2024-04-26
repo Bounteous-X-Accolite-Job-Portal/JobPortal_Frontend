6dc03826e77b3f64cf0e76f83f654e4893d3baa3
Ravi <ravi.prasad@accolitedigital.com>
2024-04-26 14:03:40 +0530

    Logo href solved

--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RegisClass } from '../../Models/registerUser';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+    public RegObj: RegisClass = new RegisClass;
+    constructor(){
+      
+    }
 
+    ngOnInit():void{
+
+    }
 }
