ef7c9a2827c4a7e87776cbb515eede05110f5c47
Apeksha Kumawat <apeksha.kumawat@accolitedigital.com>
2024-04-29 16:36:38 +0530

    Connection ongoing

--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -1,25 +1,91 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { HttpHeaders } from '@angular/common/http';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RegisClass } from '../../Models/registerUser';
+import { AuthService } from '../../Services/auth.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [HttpClientModule, FormsModule],
+  imports: [HttpClientModule, FormsModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
+  providers: [AuthService],
 })
-export class RegisterComponent implements OnInit{
-    public RegObj: RegisClass = new RegisClass();
-    constructor(){
-      
-    }
-    onSignup(){
-      console.log(this.RegObj);
-    }
+export class RegisterComponent implements OnInit {
+  //public RegObj: RegisClass = new RegisClass();
+  constructor(
+    private authService: AuthService,
+    private fb: FormBuilder,
+    private http: HttpClient
+  ) {}
 
-    ngOnInit():void{
+  registerForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    confirmPassword: ['', Validators.required],
+  });
 
+  // onSignup() {
+  //   this.authService.register(registerUser).subscribe((res) => {
+  //     //console.log(this.res);
+  //   })
+  // }
+
+  //   onSubmitRegister(): void {
+  //     const formData = this.registerForm.value;
+  // const registerData: string[] = [
+  //   formData.firstName || '',
+  //   formData.lastName || '',
+  //   formData.email || '',
+  //   formData.password || ''
+  //   // Add more fields if needed
+  // ];
+  //     this.authService.register(registerData): Observable<any> {
+  //       // Handle response here
+  //       console.log(res);
+  //     }
+  //   }
+
+  ngOnInit(): void {}
+
+  onSubmitRegister(): void {
+    if (this.registerForm.valid) {
+      const registerData = {
+        firstName: this.registerForm.value.firstName,
+        lastName: this.registerForm.value.lastName,
+        email: this.registerForm.value.email,
+        password: this.registerForm.value.password,
+      };
+
+      // this.authService
+      //   .registerUser(registerData)
+      //   .then((response) => {
+      //     console.log('Registration successful:', response.data);
+      //   })
+      //   .catch((error) => {
+      //     console.log(error);
+      //   });
+      this.authService.registerUser(registerData).subscribe(
+        (res) => {
+          // Handle successful registration
+          console.log('Registration successful:', res);
+        },
+        (error) => {
+          // Handle registration error
+          console.error('Registration error:', error);
+        }
+      );
+    } else {
+      // Form is invalid, handle accordingly
     }
+  }
 }
--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,26 +1,77 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { EmailValidator } from '@angular/forms';
+
+import axios from 'axios';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private baseURL = 'https://localhost:44372/api';
+  private baseURL = 'http://localhost:5234/api/';
 
   constructor(private http: HttpClient) {}
 
-  // register(registerUser:Array<String>) {
-  //   this.http.post(this.baseURL + 'Account/register',
-  //     {
-  //       firstName = registerUser[0],
-  //       lastName = registerUser[1],
+  // registerUser(registerData: any) {
+  //   const data = {
+  //     firstName: registerData[0],
+  //     lastName: registerData[1],
+  //     email: registerData[2],
+  //     password: registerData[3],
+  //   };
 
+  //   return axios.post(
+  //     'http://localhost:5234/api/Account/register',
+  //     {
+  //       data,
   //     },
+  //     {
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         'User-Agent': 'http://localhost:4200/',
+  //       },
+  //     }
   //   );
   // }
 
+  register(registerUser: Array<String>) {
+    // Implement your registration logic here
+    // For example:
+    // return this.http.post<any>(this.baseURL + 'Account/register', registerUser);
+    this.http.post(
+      this.baseURL + 'Account/register',
+      {
+        firstName: registerUser[0],
+        lastName: registerUser[1],
+        email: registerUser[2],
+        password: registerUser[3],
+        confirmPassword: registerUser[4],
+      },
+      {
+        responseType: 'text',
+        // firstName = registerUser[0],
+        // lastName = registerUser[1],
+      }
+    );
+  }
   login(email: any, password: any) {
     return this.http.get(this.baseURL + 'login');
+  }
+
+  registerUser(registerData: any): Observable<any> {
+    // Assuming you are making an HTTP POST request to the backend API
+    let body = registerData;
+    let headers = new Headers({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'POST',
+      'Access-Control-Allow-Headers': '*',
+    });
+
+    return this.http.post<any>(
+      'http://localhost:5234/api/Account/register',
+      body
+    );
   }
 }
