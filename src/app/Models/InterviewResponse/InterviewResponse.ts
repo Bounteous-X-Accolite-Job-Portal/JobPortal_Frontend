d9943f3e43a22d601fb132c8df328d66f38c4495
Vishal Kumar Sahu (INT1587) <vishal.sahu@accolitedigital.com>
2024-05-10 23:04:26 +0530

    Added interview feedback

--- src/app/Models/InterviewResponse/InterviewResponse.ts
+++ src/app/Models/InterviewResponse/InterviewResponse.ts
@@ -3,11 +3,13 @@ import { Guid } from "guid-typescript"
 export interface interviewResponse{
     status: number,
     message: string,
+    interviewId: Guid,
     applicationId: Guid,
     closedApplicationId: Guid,
     interviewDate: Date,
     interviewTime: string,
     interViewerId: Guid,
     link: string,
+    feedbackId?: Guid,
     empId: Guid
 }
