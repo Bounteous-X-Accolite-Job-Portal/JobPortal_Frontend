9c8da3b5c817efb72512db49138174a50ea9078e
Vishal Kumar Sahu (INT1587) <vishal.sahu@accolitedigital.com>
2024-05-28 11:09:31 +0530

    Added show more feature, job details of closed job, and improved chageStatus feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -107,6 +107,10 @@ export const routes: Routes = [
     component: JobdetailsComponent,
   },
   {
+    path: 'closedJob/job-details/:closedJobId',
+    component: JobdetailsComponent,
+  },
+  {
     path: 'user-profile',
     component: UserProfileComponent,
   },
