226a075a11182e3febeafa2406460b4c757bd45e
ApekshaKumawat <apeksha.kumawat@accolitedigital.com>
2024-05-28 11:18:40 +0530

    Spinner added

--- src/app/Components/Employee/crud-location-job-data/crud-location-job-data.component.ts
+++ src/app/Components/Employee/crud-location-job-data/crud-location-job-data.component.ts
@@ -5,6 +5,8 @@ import { CrudJobDataService } from '../../../Services/CrudJobData/crud-job-data.
 
 import { RouterLink } from '@angular/router';
 import { JobService } from '../../../Services/Job/job.service';
+import { SpinnerService } from '../../../Services/spinner.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-crud-location-job-data',
@@ -17,32 +19,44 @@ export class CrudLocationJobDataComponent {
 
   locations: location[] = [];
 
-  constructor(private jobService: JobService, private crudJobDataService: CrudJobDataService){}
+  constructor(private jobService: JobService,
+     private crudJobDataService: CrudJobDataService,
+     private toastr: ToastrService,
+    private spinnerService: SpinnerService){}
   ngOnInit(){
     this.loadJobLocations();
   }
 
   deleteByLocationId(locationId: string) : void{
+    this.spinnerService.showSpinner();
     this.crudJobDataService.deleteLocationByLocationId(locationId).subscribe(
       (res) => {
-        console.log(res);
-        console.log("Location Deleted!");
+        // console.log(res);
+        // console.log("Location Deleted!");
+        
         this.loadJobLocations();
+        this.toastr.success("Location deleted succesfully!!");
+        this.spinnerService.hideSpinner();
       },
       (error) => {
-        console.error('Error deleting location:', error);
+        // console.error('Error deleting location:', error);
+        this.spinnerService.hideSpinner();
+        this.toastr.error('Error deleting location:', error);
       }
     );
   }
   
   private loadJobLocations(): void {
+    this.spinnerService.showSpinner();
     this.jobService.getAllJobLocations().subscribe(
       (res) => {
         this.locations = res.allJobLocations;
-        console.log(this.locations);
+        this.spinnerService.hideSpinner();
+        // console.log(this.locations);
       },
       (error) => {
-        console.error('Error loading job locations:', error);
+        this.spinnerService.hideSpinner();
+        this.toastr.error('Error loading job locations:', error);
       }
     );
   }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  baseURL: 'https://localhost:7283/api/',
-  // baseURL: 'https://bountousxaccolitejobportal.azurewebsites.net/api/'
+  // baseURL: 'https://localhost:7283/api/',
+  baseURL: 'https://bountousxaccolitejobportal.azurewebsites.net/api/'
 };
