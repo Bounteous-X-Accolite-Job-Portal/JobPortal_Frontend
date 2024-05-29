98534272424fd8fa4c4032f618e2f9a02df7f03b
Vishal Kumar Sahu (INT1587) <vishal.sahu@accolitedigital.com>
2024-05-29 12:29:57 +0530

    Deployed using CI/CD pilpelines in azure

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   // baseURL: 'https://localhost:7283/api/',
-  baseURL: 'https://bountousxaccolitejobportal.azurewebsites.net/api/'
+  // baseURL: 'https://bountousxaccolitejobportal.azurewebsites.net/api/',
+  baseURL: 'https://accolite-jobportal.azurewebsites.net/api/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    baseURL: 'https://bountousxaccolitejobportal.azurewebsites.net/api/'
+    // baseURL: 'https://bountousxaccolitejobportal.azurewebsites.net/api/',
+    baseURL: 'https://accolite-jobportal.azurewebsites.net/api/'
 };
