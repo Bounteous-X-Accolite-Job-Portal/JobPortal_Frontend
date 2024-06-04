b5ade5876e0460cad62f8d15524983b41ff81352
Ankit Garg <ankit.garg@accolitedigital.com>
2024-06-04 12:06:39 +0530

    Job disabled updated

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
- baseURL: 'https://localhost:7283/api/',
- // baseURL: 'https://bountousxaccolitejobportal.azurewebsites.net/api/',
+ //baseURL: 'https://localhost:7283/api/',
+  baseURL: 'https://bountousxaccolitejobportal.azurewebsites.net/api/',
   // baseURL: 'https://accolite-jobportal.azurewebsites.net/api/'
 };
