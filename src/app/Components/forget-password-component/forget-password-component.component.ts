c95a6d34baf3d2b6a288a3dd501f703ef921c02e
ApekshaKumawat <apeksha.kumawat@accolitedigital.com>
2024-06-04 12:47:03 +0530

    CSS improved

--- src/app/Components/forget-password-component/forget-password-component.component.ts
+++ src/app/Components/forget-password-component/forget-password-component.component.ts
@@ -2,22 +2,23 @@ import { Component, Inject, OnInit, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router' ;
 import { ActivatedRoute } from '@angular/router';
 import { ResetPassword } from '../../Models/ResetPassword';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ToastrModule, ToastrService } from 'ngx-toastr';
 import { error } from 'console';
 import { ChangePasswordService } from '../../Services/ChangePassword/change-password.service';
 @Component({
   selector: 'app-forget-password-component',
   standalone: true,
-  imports: [FormsModule, ToastrModule, RouterLink],
+  imports: [FormsModule, ToastrModule, RouterLink, ReactiveFormsModule],
   templateUrl: './forget-password-component.component.html',
   styleUrl: './forget-password-component.component.css'
 })
 export class ForgetPasswordComponentComponent implements OnInit {
-  newPassword: string = '';
-  confirmPassword: string = '';
-  resetPasswordObj:ResetPassword=new ResetPassword();
+  // newPassword: string = '';
+  // confirmPassword: string = '';
+  // resetPasswordObj:ResetPassword=new ResetPassword();
   email: string | undefined;
+  forgetPasswordForm!: FormGroup;
 
   constructor(
     private resetPasswordService: ChangePasswordService,
@@ -29,24 +30,47 @@ export class ForgetPasswordComponentComponent implements OnInit {
     {
       this.route.queryParams.subscribe((params) => {
         this.email = params['email'];
-        this.resetPasswordObj.email = this.email;
       });
+
+      this.forgetPasswordForm = new FormGroup(
+        {
+          newPassword: new FormControl('', Validators.required),
+          confirmPassword: new FormControl('', Validators.required)
+        }
+    )}
+
+    passwordMatchValidator(formGroup: FormGroup): any {
+      const passwordControl = formGroup.get('password');
+      const confirmPasswordControl = formGroup.get('confirmPassword');
+  
+      if (passwordControl && confirmPasswordControl) {
+        const password = passwordControl.value;
+        const confirmPassword = confirmPasswordControl.value;
+  
+        if (password !== confirmPassword && confirmPassword !== '') {
+          confirmPasswordControl.setErrors({ passwordMismatch: true });
+          return { passwordMismatch: true };
+        } else {
+          confirmPasswordControl.setErrors(null);
+          return null;
+        }
+      } else {
+        return null;
+      }
     }
 
     resetPassword()
     {
-      if(this.resetPasswordObj.newPassword !== this.resetPasswordObj.confirmPassword) 
+      if(this.forgetPasswordForm.controls['newPassword'] !== this.forgetPasswordForm.controls['confirmPassword']) 
         {
           this.toaster.error("New Password & Confirm Password didn't match!!")
           return;
         }
 
-      this.resetPasswordService.resetPassword(this.resetPasswordObj).subscribe(
+      this.resetPasswordService.resetPassword(this.forgetPasswordForm.value).subscribe(
         (res)=>{
-          // console.log(res);
-          this.confirmPassword='';
-          this.newPassword='';
           this.toaster.success("Password Reset Successfully!!");
+          this.forgetPasswordForm.reset();
           this.router.navigate(['/login']);
           
         },
---
+++ src/app/change-password.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ChangePasswordService {
+  constructor(private http: HttpClient) {}
+  sendChangePasswordLink(email: string) {
+    return this.http.post<any>(
+      environment.baseURL + 'ChangePassword/ChangePasswordEmail' + `/${email}`,
+      {}
+    );
+  }
+
+  changePassword(resetPasswordObj: any) {
+    return this.http.post<any>(
+      environment.baseURL + 'ChangePassword/ChangePassword',
+      resetPasswordObj
+    );
+  }}
