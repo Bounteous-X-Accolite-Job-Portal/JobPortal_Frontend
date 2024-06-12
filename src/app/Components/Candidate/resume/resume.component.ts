08368a92bfbf998b8f68b72d9460df3c53808b25
ApekshaKumawat <apeksha.kumawat@accolitedigital.com>
2024-06-12 16:08:10 +0530

    Improvement

--- src/app/Components/Candidate/resume/resume.component.ts
+++ src/app/Components/Candidate/resume/resume.component.ts
@@ -6,11 +6,10 @@ import { UserStoreService } from '../../../Services/user-store.service';
 import { AuthService } from '../../../Services/auth.service';
 import { ToastrService } from 'ngx-toastr';
 import { ResumeServiceService } from '../../../Services/ResumeService/resume-service.service';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { SpinnerService } from '../../../Services/spinner.service';
 import { HttpClient } from '@angular/common/http';
-import { Console, error } from 'console';
 
 @Component({
   selector: 'app-resume',
@@ -20,24 +19,22 @@ import { Console, error } from 'console';
   styleUrl: './resume.component.css',
 })
 export class ResumeComponent {
-
-  userId: string = "";
+  userId: string = '';
   resumeExists: boolean = false;
-  msg: string = "";
+  msg: string = '';
   // resumeForm!: FormGroup;
   userResume!: Resume;
-  showRes:boolean=false;
+  showRes: boolean = false;
 
-  resumeUrl: string = "";
-  urlSafe: SafeResourceUrl ="";
+  resumeUrl: string = '';
+  urlSafe: SafeResourceUrl = '';
   file: File | null = null;
   uploading = false;
   uploadComplete = false;
   cldResponse: any;
-  temp : string = "";
+  temp: string = '';
   // productImageUrl: string | undefined;
   @Output() close = new EventEmitter<void>();
-
 
   constructor(
     private resumeService: ResumeServiceService,
@@ -48,19 +45,16 @@ export class ResumeComponent {
     private sanitizer: DomSanitizer,
     private spinner: SpinnerService,
     private http: HttpClient
-  ) { }
-
+  ) {}
 
   ngOnInit(): void {
-    this.userStore.getIdFromStore()
-      .subscribe((val) => {
-        // console.log(val);
-        let idFromToken = this.auth.getIdFromToken();
-        // console.log(idFromToken);
-        this.userId = val || idFromToken;
-        // console.log("Logged User Id : ",this.userId);
-      })
-
+    this.userStore.getIdFromStore().subscribe((val) => {
+      // console.log(val);
+      let idFromToken = this.auth.getIdFromToken();
+      // console.log(idFromToken);
+      this.userId = val || idFromToken;
+      // console.log("Logged User Id : ",this.userId);
+    });
 
     // this.resumeForm = this.fb.group({
     //   resumeUrl:['']
@@ -69,10 +63,9 @@ export class ResumeComponent {
     this.getFunction();
   }
 
-  resToggle(){
-    this.showRes=!this.showRes;
+  resToggle() {
+    this.showRes = !this.showRes;
   }
-
 
   getFunction() {
     this.spinner.showSpinner();
@@ -81,16 +74,19 @@ export class ResumeComponent {
         console.log(res);
         this.msg = res.message;
         if (res.resume == null) {
-          this.toastr.info("No resume present");
+          this.toastr.info('No resume present');
           this.resumeExists = false;
-          this.userResume = { "resumeUrl": '', "resumeId": '', "candidateId": '' };
-        }
-        else {
+          this.userResume = { resumeUrl: '', resumeId: '', candidateId: '' };
+        } else {
           // this.toastr.success("Resume successfully retrieved");
           this.resumeExists = true;
           this.userResume = res.resume;
           // this.resumeUrl = this.userResume.resumeUrl;
-          this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.userResume.resumeUrl !==undefined ? this.userResume.resumeUrl: "");
+          this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(
+            this.userResume.resumeUrl !== undefined
+              ? this.userResume.resumeUrl
+              : ''
+          );
         }
         this.spinner.hideSpinner();
         // this.resumeForm.get('resumeUrl')?.setValue(this.userResume.resumeUrl || '');
@@ -98,53 +94,52 @@ export class ResumeComponent {
       (error) => {
         this.spinner.hideSpinner();
         console.log(error);
-        this.toastr.error("Error in retrieving resume");
+        this.toastr.error('Error in retrieving resume');
       }
-    )
+    );
   }
 
   onDel() {
     this.resumeExists = !this.resumeExists;
   }
 
-
   add() {
     // console.log(this.resumeForm.value);
     this.resumeService.addResumeByCandidateId(this.resumeUrl).subscribe(
       (res) => {
         console.log(res);
-        this.toastr.success("Resume link added");
+        this.toastr.success('Resume link added');
         this.ngOnInit();
       },
       (error) => {
-        console.log("Error in adding resume link", error);
-        this.toastr.error("Could not add resume");
+        console.log('Error in adding resume link', error);
+        this.toastr.error('Could not add resume');
       }
     );
   }
 
-
   update() {
     // this.userResume.resumeUrl=this.resumeForm.value;
     // console.log(this.resumeForm.value);
-    this.resumeService.removeResumeByResumeId(this.userResume.resumeId).subscribe(
-      (res) => {
-        console.log(res);
+    this.resumeService
+      .removeResumeByResumeId(this.userResume.resumeId)
+      .subscribe(
+        (res) => {
+          console.log(res);
 
-        if(res.status === 200){
-          this.uploadFile();
+          if (res.status === 200) {
+            this.uploadFile();
+          } else {
+            this.toastr.error(res.message);
+          }
+          // this.add();
+          // this.toastr.success("Resume linked updated successfully");
+        },
+        (error) => {
+          console.log(error);
+          this.toastr.error('Error in updating resume');
         }
-        else{
-          this.toastr.error(res.message);
-        }
-        // this.add();
-        // this.toastr.success("Resume linked updated successfully");
-      },
-      (error) => {
-        console.log(error);
-        this.toastr.error("Error in updating resume")
-      }
-    );
+      );
   }
 
   handleFileChange(event: any): void {
@@ -155,11 +150,13 @@ export class ResumeComponent {
     if (!this.file) {
       return;
     }
+
     const uniqueUploadId = this.generateUniqueUploadId();
     const chunkSize = 5 * 1024 * 1024;
     const totalChunks = Math.ceil(this.file.size / chunkSize);
     let currentChunk = 0;
     this.uploading = true;
+
     const uploadChunk = async (start: number, end: number): Promise<void> => {
       const formData = new FormData();
       formData.append('file', this.file!.slice(start, end));
@@ -167,11 +164,12 @@ export class ResumeComponent {
       formData.append('upload_preset', 'jobPortal_pdf');
       formData.append('folder', 'pdf');
 
-      console.log("file info ", this.file!.slice(start, end));
+      console.log('file info ', this.file!.slice(start, end));
 
       const contentRange = `bytes ${start}-${end - 1}/${this.file!.size}`;
       console.log(
-        `Uploading chunk for uniqueUploadId: ${uniqueUploadId}; start: ${start}, end: ${end - 1
+        `Uploading chunk for uniqueUploadId: ${uniqueUploadId}; start: ${start}, end: ${
+          end - 1
         }`
       );
       try {
@@ -199,11 +197,13 @@ export class ResumeComponent {
           // console.log(this.cldResponse.url);
           //  console.log((this.cldResponse.url))
           this.temp = this.cldResponse.url;
-          this.temp = this.temp.replace("http://", "https://");
-          
+          this.temp = this.temp.replace('http://', 'https://');
+
           this.userResume.resumeUrl = this.temp;
           this.resumeUrl = this.temp;
-          this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.temp);
+          this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(
+            this.temp
+          );
           this.add();
           this.toastr.success('File uploaded', undefined, { timeOut: 5000 });
         }
@@ -212,10 +212,12 @@ export class ResumeComponent {
         this.uploading = false;
       }
     };
+
     const start = 0;
     const end = Math.min(chunkSize, this.file.size);
     uploadChunk(start, end);
   }
+
   generateUniqueUploadId(): string {
     return `uqid-${Date.now()}`;
   }
@@ -223,5 +225,4 @@ export class ResumeComponent {
   safe() {
     return this.sanitizer.bypassSecurityTrustUrl(this.temp);
   }
-
 }
